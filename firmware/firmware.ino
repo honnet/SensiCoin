96d738735dd6160dee1b5fd8be902d5b778869f4
Cedric Honnet <honnet@telecom-paristech.org>
2018-12-24 18:45:53 +0100

    Add IMU: send accelerometer and gyroscope data.

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,24 +1,36 @@
 #include <BLEPeripheral.h>
 #include "BLESerial.h"
 
+#include <Wire.h>
+#include <BMI160Gen.h>
+
 BLESerial BLESerial;
 
 const int led = 23;
 
 void setup() {
+    // initialize LEDs
     pinMode(led, OUTPUT);
     for (int i = 0; i < 9; i++) {
         digitalWrite(led, !digitalRead(led));
-        delay(30);
+        delay(60);
     }
 
-    // Custom UART test pins: 22 is actually the button...
-    Serial.setPins(0, 22); // ...and RX is not used here
+    // initialize UART
+    Serial.setPins(6, 12); // RX is not used here (6 is a random unused pin)
     Serial.begin(115200);
+    while (!Serial); // wait...
     Serial.println("Starting...");
 
+    // initialize BLE
     BLESerial.setLocalName("UART");
     BLESerial.begin();
+
+    // initialize IMU
+    pinMode(29 , OUTPUT);  // pin 29 = SDO...
+    digitalWrite(29, LOW); // ...to select default I2C address
+    Wire.setPins(30, 0);   // SDA, SCL
+    BMI160.begin(BMI160GenClass::I2C_MODE);
 }
 
 void loop() {
@@ -26,7 +38,7 @@ void loop() {
 
     forward();
     loopback();
-    spam();
+    sendIMUdata();
 }
 
 
@@ -47,16 +59,68 @@ void loopback() {
     }
 }
 
-// periodically sent time stamps
-void spam() {
-    if (BLESerial) {
-        static int old = 0;
-        int sec = millis() / 1000;
+// periodically sent IMU data
+void sendIMUdata() {
+    static int old_ms = 0;
+    const long period_ms = 100;
+    long now_ms = millis();
 
-        if (old != sec) {
-            old = sec;
-            BLESerial.print(sec);
-            digitalWrite(led, !digitalRead(led));
+    if (now_ms >= old_ms + period_ms) {
+        old_ms = now_ms;
+
+        int axRaw, ayRaw, azRaw, gxRaw, gyRaw, gzRaw; // raw values
+        float ax, ay, az, gx, gy, gz;
+
+        // read raw gyro measurements from device
+        BMI160.readMotionSensor(axRaw, ayRaw, azRaw, gxRaw, gyRaw, gzRaw);
+        now_ms = millis(); // refresh
+
+        // convert the raw accel data to g
+        ax = convertRawAccel(axRaw);
+        ay = convertRawAccel(ayRaw);
+        az = convertRawAccel(azRaw);
+        // convert the raw gyro data to degrees/second
+        gx = convertRawGyro(gxRaw);
+        gy = convertRawGyro(gyRaw);
+        gz = convertRawGyro(gzRaw);
+
+        // display values
+        Serial.print("t:\t");   Serial.print(now_ms);
+
+        Serial.print("\ta:\t"); Serial.print(ax);
+        Serial.print("\t");     Serial.print(ay);
+        Serial.print("\t");     Serial.print(az);
+
+        Serial.print("\tg:\t"); Serial.print(gx);
+        Serial.print("\t");     Serial.print(gy);
+        Serial.print("\t");     Serial.print(gz);
+        Serial.println();
+
+        if (BLESerial) {
+            BLESerial.print("t:\t");   BLESerial.print(now_ms);
+
+            BLESerial.print("\ta:\t"); BLESerial.print(ax);
+            BLESerial.print("\t");     BLESerial.print(ay);
+            BLESerial.print("\t");     BLESerial.print(az);
+
+            BLESerial.print("\tg:\t"); BLESerial.print(gx);
+            BLESerial.print("\t");     BLESerial.print(gy);
+            BLESerial.print("\t");     BLESerial.print(gz);
+            BLESerial.println();
         }
+        digitalWrite(led, !digitalRead(led));
     }
 }
+
+float convertRawAccel(int aRaw) {
+  // we use the +/- 2g range (maps to a raw value of +/- 2**15)
+  float a = (aRaw * 2.0) / (1<<15);
+  return a;
+}
+
+float convertRawGyro(int gRaw) {
+  // we use the +/- 250 degrees/seconds range (maps to +/- 2**15)
+  float g = (gRaw * 250.0) / (1<<15);
+  return g;
+}
+
